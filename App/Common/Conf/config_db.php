0f3e169d7cdae41cc3860e882f36344c75d6642f
lisar <lisar@163.com>
2017-09-11 09:52:00 +0800

    del db

--- App/Common/Conf/config_db.php
+++ App/Common/Conf/config_db.php
@@ -5,7 +5,7 @@
 return array(
     'DB_TYPE' => 'mysql', //数据库类型
     'DB_HOST' => 'localhost', //数据库主机
-    'DB_NAME' => 'jjtest_jjkio', //数据库名称
+    'DB_NAME' => 'jjtest', //数据库名称
     'DB_USER' => 'root', //数据库用户名
     'DB_PWD' => 'root', //数据库密码
     'DB_PORT' => '3306', //数据库端口
